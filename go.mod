ab87916c2fab7f50775a64ba972124dbc81e9ff7
flipfloppy1 <flipfloppy.business@gmail.com>
2024-11-18 23:12:31 +1030

    made the game work lol

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module flipfloppy/gomine
 go 1.23.2
 
 require (
-	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	github.com/eiannone/keyboard v0.0.0-20220607221743-a9f4cc43507a
 	golang.org/x/sys v0.26.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"os/exec"
 	"runtime"
 	"slices"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/eiannone/keyboard"
@@ -24,6 +26,8 @@ var leftKeys = []keyboard.Key{keyboard.KeyArrowLeft, keyboard.KeyArrowUp}
 var leftRunes = []rune{'h', 'j'}
 var rightKeys = []keyboard.Key{keyboard.KeyArrowRight, keyboard.KeyArrowDown}
 var rightRunes = []rune{'k', 'l'}
+var actionRunes = []rune{'h', 'j', 'k', 'l', 'c', 'm'}
+var actionKeys = []keyboard.Key{keyboard.KeyArrowLeft, keyboard.KeyArrowRight, keyboard.KeyArrowUp, keyboard.KeyArrowDown, keyboard.KeySpace, keyboard.KeyEnter}
 
 var d = rand.NewPCG(uint64(time.Now().Unix()), uint64(time.Now().UnixMicro()))
 var rng = rand.New(d)
@@ -58,12 +62,56 @@ func CheckQuit(char rune, key keyboard.Key) {
 	}
 }
 
+var (
+	keyBuffMut sync.Mutex
+	keyBuffUse bool
+)
+
+type cursor_mode int
+
+const (
+	CLEAR = iota
+	MARK
+)
+
+type currKey struct {
+	keyVal keystroke
+	fired  bool
+}
+
+type position struct {
+	x int
+	y int
+}
+
+type cursor struct {
+	pos  position
+	mode cursor_mode
+}
+
+type keystroke struct {
+	char rune
+	key  keyboard.Key
+}
+
+func AddKeystroke(buff *currKey) {
+	char, key, _ := keyboard.GetKey()
+	keyBuffMut.Lock()
+	if buff.fired {
+		buff.keyVal = keystroke{char, key}
+		buff.fired = false
+	}
+	keyBuffMut.Unlock()
+}
+
 func main() {
 	clear()
 	fmt.Println("Minesweeper written in Go - Q to quit")
 	fmt.Println("Choose game type:")
 
 	selectedDifficulty = 0
+
+	keyboard.Open()
 
 	PrintDifficulties()
 	PrintSelector(difficulties, 1, selectedDifficulty)
@@ -96,18 +144,184 @@ func main() {
 	}
 
 	clear()
-	PrintMinefield(CreateMinefield(width, height, mineNum))
+	field := CreateMinefield(width, height, mineNum)
+	cur := cursor{position{int(width / 2), int(height / 2)}, CLEAR}
+	frameNum := uint(0)
+
+	var repeatStack string = ""
+
+	var startTime time.Time = time.Now()
+	var endTime time.Time = time.Now()
+
+	var keyBuffer = new(currKey)
+	(*keyBuffer).fired = true
+
+	gameEnd := false
+	endMessage := ""
+
+	go func(*currKey) {
+		for !gameEnd {
+			AddKeystroke(keyBuffer)
+			time.Sleep(time.Millisecond * 1)
+		}
+	}(keyBuffer)
+
+	cursorDrawn := true
+
+	for !gameEnd {
+		startTime = time.Now()
+
+		update := false
+		keystroke := keystroke{}
+		keyBuffMut.Lock()
+		if !keyBuffer.fired {
+			keystroke = keyBuffer.keyVal
+			keyBuffer.fired = true
+			update = true
+		}
+		keyBuffMut.Unlock()
+
+		if update {
+			char := keystroke.char
+			key := keystroke.key
+
+			CheckQuit(char, key)
+
+			if char >= '0' && char <= '9' {
+				repeatStack += string(char)
+			}
+
+			numRep := int(1)
+			if repeatStack != "" {
+				numTmp, err := strconv.Atoi(repeatStack)
+				if err == nil {
+					numRep = max(numTmp, 1)
+				}
+			}
+
+			if slices.Contains(actionKeys, key) {
+				switch key {
+				case keyboard.KeyArrowLeft:
+					cur.pos.x = max(cur.pos.x-numRep, 0)
+					break
+				case keyboard.KeyArrowRight:
+					cur.pos.x = min(cur.pos.x+numRep, int(width-1))
+					break
+				case keyboard.KeyArrowDown:
+					cur.pos.y = min(cur.pos.y+numRep, int(height-1))
+					break
+				case keyboard.KeyArrowUp:
+					cur.pos.y = max(cur.pos.y-numRep, 0)
+					break
+				case keyboard.KeySpace, keyboard.KeyEnter:
+					if cur.mode == CLEAR {
+						field.area[cur.pos.x][cur.pos.y].isCleared = true
+						if field.area[cur.pos.x][cur.pos.y].hasMine {
+							gameEnd = true
+							endMessage = "You lose! :("
+						}
+					} else if cur.mode == MARK {
+						field.area[cur.pos.x][cur.pos.y].hasMark = true
+					}
+					break
+				}
+			}
+
+			if slices.Contains(actionRunes, char) {
+				switch char {
+				case 'h':
+					cur.pos.x = max(cur.pos.x-numRep, 0)
+					break
+				case 'l':
+					cur.pos.x = min(cur.pos.x+numRep, int(width-1))
+					break
+				case 'j':
+					cur.pos.y = min(cur.pos.y+numRep, int(height-1))
+					break
+				case 'k':
+					cur.pos.y = max(cur.pos.y-numRep, 0)
+					break
+				case 'c':
+					cur.mode = CLEAR
+					break
+				case 'm':
+					cur.mode = MARK
+				}
+			}
+
+			if slices.Contains(actionKeys, key) || slices.Contains(actionRunes, char) {
+				repeatStack = ""
+			}
+		}
+		endTime = time.Now()
+		time.Sleep(startTime.Sub(endTime) + time.Millisecond*20)
+		if frameNum%20 == 0 {
+			cursorDrawn = !cursorDrawn
+		}
+
+		if update || frameNum%20 == 0 {
+			clear()
+			PrintMinefield(field, cur, cursorDrawn)
+			if cur.mode == CLEAR {
+				fmt.Print("CLEAR MODE: (c)")
+			} else {
+				fmt.Print("MARK MODE: (m)")
+			}
+			if repeatStack != "" {
+				fmt.Print(" - ", repeatStack)
+			}
+			fmt.Print("\n")
+		}
+		frameNum++
+	}
+
+	clear()
+	for x := range field.area {
+		for y := range field.area[x] {
+			if field.area[x][y].hasMine {
+				field.area[x][y].isCleared = true
+			}
+		}
+	}
+	PrintMinefield(field, cur, false)
+	fmt.Println(endMessage)
 
 }
 
-func PrintMinefield(field minefield) {
+func GetPlotChar(plot plot) string {
+	var char string = ""
+
+	if plot.isCleared {
+		if plot.hasMine {
+			char = "*"
+		} else {
+			char = strconv.Itoa(int(plot.mineNum))
+		}
+	} else if plot.hasMark {
+		char = "!"
+	} else {
+		char = "#"
+	}
+
+	return char
+}
+
+func PrintMinefield(field minefield, cur cursor, cursorDrawn bool) {
 	mineStrings := make([]string, field.height, field.height)
-	for _, v := range field.hasMine {
-		for y, b := range v {
-			if b {
-				mineStrings[y] += "."
+	for x, v := range field.area {
+		for y, plot := range v {
+			if cur.pos.x == x && cur.pos.y == y {
+				if cursorDrawn {
+					if cur.mode == CLEAR {
+						mineStrings[y] += "c"
+					} else {
+						mineStrings[y] += "m"
+					}
+				} else {
+					mineStrings[y] += GetPlotChar(plot)
+				}
 			} else {
-				mineStrings[y] += "#"
+				mineStrings[y] += GetPlotChar(plot)
 			}
 		}
 	}
@@ -116,31 +330,67 @@ func PrintMinefield(field minefield) {
 	}
 }
 
+type plot struct {
+	hasMine   bool
+	isCleared bool
+	hasMark   bool
+	mineNum   uint
+}
+
 type minefield struct {
 	width   uint
 	height  uint
 	mineNum uint
-	hasMine [][]bool
+	area    [][]plot
 }
 
 func CreateMinefield(width uint, height uint, mineNum uint) minefield {
-	hasMine := make([][]bool, width, width)
-	for i := range hasMine {
-		hasMine[i] = make([]bool, height, height)
+	area := make([][]plot, width, width)
+	for i := range area {
+		area[i] = make([]plot, height, height)
 	}
 
-	field := minefield{width, height, mineNum, hasMine}
+	field := minefield{width, height, mineNum, area}
 	for x := uint(0); x < width; x++ {
 		for y := uint(0); y < height; y++ {
-			hasMine[x][y] = false
+			field.area[x][y].hasMine = false
+			field.area[x][y].hasMark = false
+			field.area[x][y].isCleared = false
+			field.area[x][y].mineNum = 0
 		}
 	}
 
+	// Randomly place mines
 	for i := uint(0); i < mineNum; i++ {
 		for {
-			if hasMine[rng.Uint()%width][rng.Uint()%height] != true {
-				hasMine[rng.Uint()%width][rng.Uint()%height] = true
+			if !field.area[rng.Uint()%width][rng.Uint()%height].hasMine {
+				field.area[rng.Uint()%width][rng.Uint()%height].hasMine = true
 				break
+			}
+		}
+	}
+
+	var offsets [][]int = [][]int{
+		{0, 1},
+		{1, 0},
+		{0, -1},
+		{-1, 0},
+		{-1, -1},
+		{1, 1},
+		{1, -1},
+		{-1, 1}}
+
+	// Count adjacent mines for each plot
+	for x := uint(0); x < width; x++ {
+		for y := uint(0); y < height; y++ {
+			for _, off := range offsets {
+				xTotal := int(x) + off[0]
+				yTotal := int(y) + off[1]
+				if !(xTotal < 0 || xTotal >= int(width) || yTotal < 0 || yTotal >= int(height)) {
+					if field.area[xTotal][yTotal].hasMine {
+						field.area[x][y].mineNum++
+					}
+				}
 			}
 		}
 	}
@@ -176,11 +426,7 @@ func ConsoleGetUint(message string, min uint, max uint) uint {
 	clear()
 	fmt.Println(message)
 	for {
-		char, key, err := keyboard.GetSingleKey()
-		if err != nil {
-			time.Sleep(time.Millisecond * 30)
-			continue
-		}
+		char, key, _ := keyboard.GetKey()
 
 		CheckQuit(char, key)
 
@@ -204,16 +450,12 @@ func ConsoleGetUint(message string, min uint, max uint) uint {
 }
 
 func GetCustomDifficulty() (uint, uint, uint) {
-	return ConsoleGetUint("Choose Width:", 0, 100), ConsoleGetUint("Choose Height:", 0, 100), ConsoleGetUint("Choose Number of Mines:", 0, 100)
+	return ConsoleGetUint("Choose Width:", 0, 100), ConsoleGetUint("Choose Height:", 0, 100), ConsoleGetUint("Choose Number of Mines:", 0, 1000)
 }
 
 func GetDifficulty() string {
 	for {
-		char, key, err := keyboard.GetSingleKey()
-		if err != nil {
-			time.Sleep(time.Millisecond * 30)
-			continue
-		}
+		char, key, _ := keyboard.GetKey()
 
 		CheckQuit(char, key)
 
